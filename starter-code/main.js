342e5324af9d20e117fc8ec0a222774379450cc9
ashleyvernon <avernon526@gmail.com>
2016-08-14 18:45:09 -0700

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,11 @@
-console.log("JS file is connected to HTML! Woo!")
+/* cards for memory game */
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if (cardTwo === cardFour){
+	alert("Sorry, try again.")
+} else (cardOne === cardTwo){
+	alert("You found a match!")
+}
